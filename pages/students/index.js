88267503f76f9ce1f09291e7006f58df4e64cec0
Ruimas-UTI <allpraisethelord@gmail.com>
2021-07-02 23:45:55 +0600

    CRUD init

--- pages/students/index.js
+++ pages/students/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { dummyStudents } from "../../staticstorage/storage"
+import { Container,List,ListSubheader } from '@material-ui/core'
 
 export default function Students() {
-    console.log(dummyStudents)
     return (
-        <div>
-            All Students will appear here
-        </div>
+        <Container>
+            <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                subheader={
+                    <ListSubheader component="div" id="nested-list-subheader">
+                    Nested List Items
+                    </ListSubheader>
+                }
+                className={classes.root}
+                >
+             </List>
+        </Container>
     )
 }
