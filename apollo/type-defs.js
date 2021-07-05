ed723f674bb44967e6629ca7cf7c9cf9290e406e
Ruimas-UTI <allpraisethelord@gmail.com>
2021-07-05 20:56:57 +0600

    last try

---
+++ apollo/type-defs.js
@@ -0,0 +1,53 @@
+import gql from 'graphql-tag'
+const typeDefs = gql`
+  type Student {
+    _id: ID!
+    name: String!
+    email: String!
+    phone: Int!
+    dateOfBirth: String!
+    subjects: [Subject]
+  }  
+  type Subject {
+      value: String!
+      label:String!
+  }
+  type Query {
+    students: [Student]!
+    subjects: [Subject]!
+    student(id: ID): [Student]
+    subject(id: ID): [Subject]
+  }
+  input StudentInput {
+    name: String!
+    email: String!
+    phone: Int!
+    dateOfBirth: String!
+    subjects: [SubjectInput]!
+  }
+  input SubjectInput {
+    value: String!
+    label:String!
+  }
+  input UpdateStudentInput {
+    name: String
+    email: String
+    phone: Int
+    dateOfBirth: String
+    subjects: [SubjectInput]
+  }
+  input UpdateSubjectInput{
+    value: String
+    label:String
+  }
+  type Mutation {
+    createStudent(student:StudentInput): Student
+    createSubject(subject:SubjectInput): Subject
+    updateStudent(id:ID, student:UpdateStudentInput): Student
+    updateSubject(id:ID, subject:UpdateSubjectInput): Subject
+    deleteStudent(id: ID): String
+    deleteSubject(id: ID): String
+  }
+`
+
+export default typeDefs
