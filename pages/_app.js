53d0ea95b35ce6b28b0c39d8a34674b818ce820e
Ruimas-UTI <allpraisethelord@gmail.com>
2021-07-05 20:06:43 +0600

    apollo client setup complete, queries remain

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apolloClient";
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
@@ -8,7 +10,7 @@ import Navbar from '../components/navbar'
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
-
+  const apolloClient = useApollo(pageProps.initialApolloState);
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -18,18 +20,20 @@ export default function MyApp(props) {
   }, []);
 
   return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Navbar/>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
+    <ApolloProvider client={apolloClient}>
+        <React.Fragment>
+            <Head>
+              <title>My page</title>
+              <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <Navbar/>
+              <Component {...pageProps} />
+            </ThemeProvider>
+        </React.Fragment>
+    </ApolloProvider>
   );
 }
 
